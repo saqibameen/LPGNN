36993ead37b24e1e87f1b31b9a48f3907483ba8c
Saqib Ameen <mrsaqibameen@gmail.com>
2021-12-02 19:37:09 -0700

    revert back to cpu

--- cora_job_array.sh
+++ cora_job_array.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-#SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=1   # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=64000M        # memory per node
-#SBATCH --time=00:30:00     # time of the task
-#SBATCH --account=def-lelis
+#SBATCH --time=03:00:00     # time of the task
+#SBATCH --account=rrg-lelis
 #SBATCH --output=%N-%j.out
 #SBATCH --mail-user=saqib1@ualberta.ca
 #SBATCH --mail-type=ALL
@@ -15,7 +14,7 @@ module load python/3.9
 module load scipy-stack
 
 source ./env/bin/activate
-module load cuda cudnn
+# module load cuda cudnn
 
 
 python3 experiments.py -n cora exec --id $SLURM_ARRAY_TASK_ID
