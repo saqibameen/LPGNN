4b9a30c661c16474c169bb49291958d071111cad
Saqib Ameen <mrsaqibameen@gmail.com>
2021-12-05 01:18:26 -0700

    Update cora_job_array.sh

--- cc_scripts/cora_job_array.sh
+++ cc_scripts/cora_job_array.sh
@@ -21,8 +21,8 @@ pip install torch --no-index
 pip install torch_sparse --no-index
 pip install torch_scatter --no-index
 pip install torch_geometric --no-index
+pip install node2vec --no-index
 pip install -r requirements.txt 
-pip install node2vec
 
 module load cuda cudnn
 
